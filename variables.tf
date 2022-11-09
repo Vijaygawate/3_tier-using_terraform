1e0436f1a84b4b81bf1c59b6829d9cdc0af335e3
Vijay Gawate <88774056+Vijaygawate@users.noreply.github.com>
2022-11-09 11:48:14 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,3 +1,6 @@
+# Author    : Vijay Gawte
+# GitHub    : https://github.com/Vijaygawate
+#####################################################
 variable "cidr" {
   type = list
   default = ["10.0.1.0/24","10.0.2.0/24"]
